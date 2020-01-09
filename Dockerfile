6125dc428618f6e53163d73e14a495881b03b38b
florian <florian@florian-VirtualBox>
2020-01-09 18:03:44 +0100

    secure

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,9 @@
 FROM alpine:latest
 RUN apk update && apk add python3
-ADD worker.py /home/
+RUN pip3 install Flask &&\
+pip3 install pika &&\
+pip3 install requests &&\
+pip3 install simplejson &&\
+pip3 install docker
+COPY worker.py /worker.py
+CMD python3 worker.py
--- worker.py
+++ worker.py
@@ -1,2 +1,82 @@
 #!/usr/bin/python3
 
+from flask import Flask, request
+import pika
+import requests
+import simplejson as json
+import docker
+
+
+#fonctions Files de messages
+
+def demandeCreationFile(nomFile,ip):
+    #x = {}
+    #x["nomFile"] = nomFile
+    #myParam={"donnees" : json.dumps(x)}
+    myParam = {"nomFile":nomFile}
+    r = requests.post("http://{}/rabbit".format(ip), data=myParam)
+    donnees = json.loads(r.text)
+    print("File {} créée".format(donnees["nomFile"]))
+
+def demandeDepotMessageDansFile(nomFile, message, ip):
+    myParam={"message":message}
+    r = requests.post("http://{}/rabbit/{}".format(ip, nomFile),data=myParam)
+    donnees = json.loads(r.text)
+    print("Message déposé : {} ".format(donnees["message"]))
+
+def lectureMessageDansFile(nomFile):
+    myParam={"nomFile":nomFile}
+    r = requests.get("http://localhost:5000/rabbit/{}".format(nomFile),params=myParam)
+    donnees = json.loads(r.text)
+    #print("Message lu : {} ".format(donnees["message"]))
+    return (format(donnees["message"]))
+
+#fonctions Docker
+def executerConteneurSimple(nomConteneur):
+    #un client doit être instantié pour communiquer avec un demon Docker
+    client = docker.from_env()
+    client.containers.run("rabbitmq", detach=True, name=nomConteneur)
+
+#path est le chemin du répertoire qui contient le dockerFile
+#tag est le nom que portera l'image résultante
+def buildImageFromDockerFile(cheminDockerFile, tagImage):
+    client = docker.from_env()
+    client.images.build(path=cheminDockerFile, tag=tagImage)
+
+#l'image est l'image créée à la suite de l'appel de la fonction du dessus
+def construireConteneurFromDockerFile(nomConteneur, image):
+    client = docker.from_env()
+    client.containers.run(image, detach=True, name=nomConteneur)
+
+def afficherConteneursEnCoursExecution():
+    client = docker.from_env()
+    for conteneur in client.containers.list():
+        print(conteneur.attrs['Name'])
+
+def demarrerConteneur(nom):
+    client = docker.from_env()
+    conteneur = client.containers.get(nom)
+    conteneur.start()
+
+def arreterConteneur(nom):
+    client = docker.from_env()
+    conteneur = client.containers.get(nom)
+    conteneur.stop()
+
+if __name__ == '__main__':
+    
+    #print("oui")
+    demandeCreationFile("fileConteneur", "172.17.0.1:5000")
+    #demandeDepotMessageDansFile("fileConteneur", "oui", "localhost:5000")
+    #print("hello")
+    #message = lectureMessageDansFile("ToDo")
+    #while(message != "Vide"):
+        #un replace a été mis en place car une erreur était générée par
+        #le json.loads -> le json lu contenait des quotes à la place
+        #des doubles quotes
+    #    tache = json.loads(message.replace("\'","\""))
+    #    test1 = tache["id_projet"]
+    #    test2 = tache["id_tache"]
+    #    print("id_projet : {}".format(test1))
+    #    print("id_tache : {}".format(test2))
+    #    message = lectureMessageDansFile("ToDo")
