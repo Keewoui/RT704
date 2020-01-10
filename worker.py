b765bb0c9464dbe0ff6a4dc60bfccb3550a0e479
florian <florian@florian-VirtualBox>
2020-01-10 20:40:49 +0100

    secure2

--- worker.py
+++ worker.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-from flask import Flask, request
-import pika
+from flask import request
 import requests
 import simplejson as json
 import docker
@@ -66,16 +65,16 @@ def arreterConteneur(nom):
 if __name__ == '__main__':
     
     #print("oui")
-    demandeCreationFile("172.17.0.1:5000", "fileConteneur")
+    #demandeCreationFile("172.17.0.1:5000", "fileConteneur")
     #demandeDepotMessageDansFile("fileConteneur", "oui", "localhost:5000")
     #print("hello script lancé !!")
-    #message = lectureMessageDansFile("172.17.0.1:5000", "ToDo")
-    #while(message != "Vide"):
+    message = lectureMessageDansFile("172.17.0.1:5000", "ToDo")
+    while(message != "Vide"):
         #un replace a été mis en place car une erreur était générée par
         #le json.loads -> le json lu contenait des quotes à la place
         #des doubles quotes
-    #    tache = json.loads(message.replace("\'","\""))
-    #    test1 = tache["id_projet"]
-    #    test = tache["id_tache"]
-    #    demandeDepotMessageDansFile("172.17.0.1:5000", "Done", "idTache : {}".format(test))
-    #    message = lectureMessageDansFile("172.17.0.1:5000", "ToDo")
+        tache = json.loads(message.replace("\'","\""))
+        test1 = tache["id_projet"]
+        test = tache["id_tache"]
+        demandeDepotMessageDansFile("172.17.0.1:5000", "Done", "idProjet : {} idTache".format(test1, test))
+        message = lectureMessageDansFile("172.17.0.1:5000", "ToDo")
